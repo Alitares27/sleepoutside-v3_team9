07caaa4aeffe575865054e97d78ed60b4a9b74bb
nicholas-28 <nika199828@mail.ru>
2023-05-05 11:47:23 +0400

    productDetails module done

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,18 +1,23 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
-// 1. productDetails(productId)
+let product = {};
 
-export default async function productDetails()
+// 1. productDetails(productId)
+export default async function productDetails(productId)
 {
-    
+    product = await findProductById(productId);
+
+    renderProductDetails();
+
+    document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
 // 2. addToCart()
-function addProductToCart(product) {
+function addToCart() {
     setLocalStorage("so-cart", product);
   }
-  // add to cart button event handler
+/*  // add to cart button event handler
   async function addToCartHandler(e) {
     const product = await findProductById(e.target.dataset.id);
     addProductToCart(product);
@@ -22,4 +27,18 @@ function addProductToCart(product) {
   document
     .getElementById("addToCart")
     .addEventListener("click", addToCartHandler);
-  
+*/
+
+//3. renderProductDetails()
+
+function renderProductDetails(){
+
+    document.querySelector("#productName").innerText = product.Brand.Name;
+    document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").alt = product.Name;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText = product.Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
+    document.querySelector("#addToCart").dataset.id = product.Id;
+}
