b5467ab73632cfacb883bc1acb38591afeb79457
WilsonGbedemah <afrogbede09@gmail.com>
2023-05-06 22:38:14 -0700

    changes

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -9,10 +9,10 @@ function convertToJson(res) {
 export function getData(category = "tents") {
   return fetch(`../json/${category}.json`)
     .then(convertToJson)
-    .then((data) => data);
+    .then((data) => data);// or witten as .then(function(data) { return data; })
 }
 
 export async function findProductById(id) {
   const products = await getData();
-  return products.find((item) => item.Id === id);
+  return products.find((item) => item.Id === id); // or written as return products.find(function(item) { return item.Id === id; })
 }
