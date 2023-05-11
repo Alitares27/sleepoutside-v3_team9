9e2c88e372739b21c0a313708ec4669c77102d5c
WilsonGbedemah <afrogbede09@gmail.com>
2023-05-10 20:09:36 -0700

    updates

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,21 +1,21 @@
 import { findProductById } from "./productData.mjs";
 import { setLocalStorage } from "./utils.mjs";
 
-let product = {};
+
 
 export default async function productDetails(productId) {
   // get the details for the current product. findProductById will return a promise! use await or .then() to process it
-   const prod = await findProductById(productId);
+   const product = await findProductById(productId);
   // once we have the product details we can render out the HTML
-  renderProductDetails();
+  renderProductDetails(product);
   // once the HTML is rendered we can add a listener to Add to Cart button
-  document.getElementById("addToCart").addEventListener("click", () => addToCart(prod));
+  document.getElementById("addToCart").addEventListener("click", () => addToCart(product));
 }
-function addToCart() {
+function addToCart(product) {
   setLocalStorage("so-cart", product);
 }
 
-function renderProductDetails() {
+function renderProductDetails(product) {
   document.querySelector("#productName").innerText = product.Brand.Name;
   document.querySelector("#productNameWithoutBrand").innerText =
     product.NameWithoutBrand;
