38039a99983473d2ed514b4aa2ac92748637750f
WilsonGbedemah <afrogbede09@gmail.com>
2023-05-19 21:38:00 -0700

    updates done

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,13 +21,20 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(param);
 }
 
-export function renderListWithTemplate(templateFn, parentElement, list, position = "afterbegin", clear = true) {
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = true
+) {
   if (clear) {
     parentElement.innerHTML = "";
   }
@@ -35,36 +42,43 @@ export function renderListWithTemplate(templateFn, parentElement, list, position
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
 }
 
-export async function renderWithTemplate(templateFn, parentElement, data, callback, position = "afterbegin", clear = true) {
+export async function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  callback,
+  position = "afterbegin",
+  clear = true
+) {
   if (clear) {
     parentElement.innerHTML = "";
   }
   const htmlString = await templateFn(data);
   parentElement.insertAdjacentHTML(position, htmlString);
-  if (callback){
+  if (callback) {
     callback(data);
   }
 }
 
- function loadTemplate(path){
+function loadTemplate(path) {
   // wait what?  we are returning a new function? this is called currying and can be very helpful.
-    return async function(){
-      const res = await fetch(path);
-      if(res.ok){
-        const html = await res.text();
-        return html;
-      }
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
     }
+  };
 }
 
-export async function loadHeaderFooter(){
+export async function loadHeaderFooter() {
   // header template will still be a function! But one where we have pre-supplied the argument.
   // headerTemplate and footerTemplate will be almost identical, but they will remember the path we passed in when we created them
   // why is it important that they stay functions?  The renderWithTemplate function is expecting a template function...if we sent it a string it would break, if we changed it to expect a string then it would become less flexible.
   const headerTemplateFn = loadTemplate("/partials/header.html");
   const footerTemplateFn = loadTemplate("/partials/footer.html");
-  const headerEl = document.querySelector("main-header");
-  const footerEl = document.querySelector("main-footer");
+  const headerEl = document.querySelector("#main-header");
+  const footerEl = document.querySelector("#main-footer");
   renderWithTemplate(headerTemplateFn, headerEl);
   renderWithTemplate(footerTemplateFn, footerEl);
 }
