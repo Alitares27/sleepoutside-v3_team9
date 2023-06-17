3d2b7fe5efb14aac13d16d10b5d93847d5de3315
Aldair Payehuanca <aldair2795@hotmail.com>
2023-06-17 16:08:09 -0500

    fixing server

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
